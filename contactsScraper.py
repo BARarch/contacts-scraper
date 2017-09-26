7f53445449aa354e1adbac78c7cb5bff2086cdba
Anthony Quivers <anthonyquivers@gmail.com>
2017-09-25 21:49:55 -0400

    More Directory Tests

--- contactsScraper.py
+++ contactsScraper.py
@@ -1,6 +1,12 @@
 import pandas as pd
 import scraperModelGS as smgs
 
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+import time
+import datetime as dt
+
 def getContacts():
     """Google Sheets API Code.
     Pulls urls for all NFL Team RSS Feeds
@@ -141,6 +147,126 @@ def sheetRecord(row, recordKeys):
     return record
 
 
+# Company Directory Manager Classes
+class DirectoryManager(object):
+    ## The goal of this class is to manage the directory in the enviroment not to be one!
+    ## the directory as well as it access and packaging functions will opporate as utility functions, these
+    ## the routines of this class will call those utility functions
+    def __init__(self, orgRecords):
+        self.orgRecords = orgRecords
+        #self.browser = webdriver.Chrome(path_to_chromedriver)
+        
+    def findOrgRecord(self, organization):
+        for org in self.orgRecords:
+            if organization == org['Organization']:
+                return org
+            
+    def get_organizations(self):
+        return [x['Organization'] for x in self.orgRecords]
+            
+    def orgRecordIndex(self, orgRecord):
+        return self.orgRecords.index(orgRecord)
+    
+    def linkList(self, orgRecord):
+        lis = [orgRecord['Directory link'], orgRecord['Link 2'], orgRecord['Link 3'], orgRecord['Link 4']]
+        return lis[:lis.index('')]
+    
+    def writeRecordRow(self, row, index):
+        """Google Sheets API Code.
+        """
+        credentials = get_credentials()
+        http = credentials.authorize(smgs.httplib2.Http())
+        discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
+                        'version=v4')
+        service = smgs.discovery.build('sheets', 'v4', http=http,
+                                  discoveryServiceUrl=discoveryUrl)
+
+        spreadsheet_id = '1p1LNyQhNhDBNEOkYQPV9xcNRe60WDlmnuiPp78hxkIs'
+        value_input_option = 'RAW'
+        rangeName = 'Org Leadership Websites!F' + str(index + 2)
+        values = row
+        body = {
+              'values': values
+        }
+
+        result = service.spreadsheets().values().update(spreadsheetId=spreadsheet_id, range=rangeName,
+                                                        valueInputOption=value_input_option, body=body).execute()
+
+        return result
+    
+
+class OrgSession(DirectoryManager):
+	chromeBrowserPath = '/Users/Anthony/scripts/Contacts-Scraper/Drivers/chromedriver' # change path as needed 
+	sessionBrowser = webdriver.Chrome(chromeBrowserPath) 
+
+	MillisecondFormatMax = 2
+
+	def __init__(self, orgRecords):
+		DirectoryManager.__init__(self, orgRecords)
+		
+        
+	def processSession(self, organization):
+		self.organization = organization
+		self.orgRecord = DirectoryManager.findOrgRecord(self, self.organization)
+		self.sessionIndex = DirectoryManager.orgRecordIndex(self, self.orgRecord)
+
+		## Get all links for Session Instance
+		self.links = DirectoryManager.linkList(self, self.orgRecord)
+
+		## Retreve Queries
+		self.orgQueries = [OrgQuery(link, OrgSession.sessionBrowser) for link in self.links]
+		    
+		## Analyze Query Session Collect Data
+		callTime = self.orgQueries[-1].get_callTimeStr()  # This is the Call time for the last link in the Session
+		totalTime = sum([query.get_responseTime() for query in self.orgQueries])
+		self.sessionStatus = 'Good'
+
+		## Set Response time in Second or Millisecond format
+		if totalTime < OrgSession.MillisecondFormatMax:
+			timeUnit = 'ms'
+			totalTimeFormat = '%.0f' % (totalTime * 1000)
+		else:
+			timeUnit = 's'
+			totalTimeFormat = '%.3f' % totalTime
+
+
+		res = DirectoryManager.writeRecordRow(self, [[callTime, totalTimeFormat, timeUnit, self.sessionStatus ]], self.sessionIndex)
+
+		## Return Query Objects
+		return self.orgQueries
+
+
+class OrgQuery(object):
+    def __init__(self, link, browser):
+        self.link = link
+        start = time.clock()
+        self.query = browser.get(link)
+        self.pageSource = browser.page_source
+        self.soup = BeautifulSoup(self.pageSource, 'lxml')
+        self.responseTime = time.clock() - start
+        self.callTime = dt.datetime.now()
+        
+    def get_query(self):
+        return self.query
+    
+    def get_pageSource(self):
+        return self.pageSource
+    
+    def get_soup(self):
+        return self.soup
+    
+    def get_responseTime(self):
+        return self.responseTime
+    
+    def get_callTime(self):
+        return self.callTime
+        
+    def get_callTimeStr(self):
+        return self.callTime.strftime('%a %b %d, %Y  %H:%M:%S')
+        
+
+
+
 
 if __name__ == '__main__':
 	# Initialize Google Sheets for Write
@@ -160,3 +286,5 @@ if __name__ == '__main__':
 	cr = pd.DataFrame(contactRecords)
 	dr = pd.DataFrame(orgRecords)
 	print('DATAFRAMES READY')
+
+	
