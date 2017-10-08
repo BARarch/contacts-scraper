1ff61296aa3da39d0f9ae58277144f7138a6c07b
BARarch <anthonyquivers@gmail.com>
2017-10-08 18:28:17 -0400

    Contact Sheet Output

--- contactChecker.py
+++ contactChecker.py
@@ -211,7 +211,6 @@ class ContactPointerFamily(object):
 		ContactPointerFamily.docSoup = soup
 
 class VerifiedPointer(ContactPointerFamily):
-	get_credentials = smgs.modelInit()
 
 	def __init__(self, rec):
 		ContactPointerFamily.__init__(self, rec)
@@ -231,3 +230,104 @@ class VerifiedPointer(ContactPointerFamily):
 	@classmethod
 	def init_Sheet_Writes(cls):
 		VerifiedPointer.get_credentials = smgs.modelInit()
+
+
+
+class VerificationHandler(object):
+
+	def __init__(self, org):
+
+class ContactSheetOutput(object):
+	get_credenitals = smgs.modelInit()
+	initialRead = read_contact_output_records()
+	initialRow = 7
+	currentRow = ContactSheetOutput.initialRow + len(ContactSheetOutput.initialRead)
+
+	def __init__(self, name):
+		self.name = name
+
+	def outputSingleRow(self, row):
+		"""Google Sheets API Code.
+		"""
+		credentials = ContactSheetOutput.get_credentials()
+		http = credentials.authorize(smgs.httplib2.Http())
+		discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
+		                'version=v4')
+		service = smgs.discovery.build('sheets', 'v4', http=http,
+		                          discoveryServiceUrl=discoveryUrl)
+
+		spreadsheet_id = '1p1LNyQhNhDBNEOkYQPV9xcNRe60WDlmnuiPp78hxkIs'
+		value_input_option = 'RAW'
+		rangeName = 'Org Leadership Websites!F' + str(ContactSheetOutput.currentRow)
+		values = row
+		body = {
+		      'values': values
+		}
+
+		try:
+			result = service.spreadsheets().values().update(spreadsheetId=spreadsheet_id, range=rangeName,
+		                                                valueInputOption=value_input_option, body=body).execute()
+		except:
+			print('Missed Row Output')
+		else:
+			ContactSheetOutput.currentRow += 1
+
+		return result 
+
+	def outputBatchRow(self, rows):
+		"""Google Sheets API Code.
+		"""
+		credentials = ContactSheetOutput.get_credentials()
+		http = credentials.authorize(smgs.httplib2.Http())
+		discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
+		                'version=v4')
+		service = smgs.discovery.build('sheets', 'v4', http=http,
+		                          discoveryServiceUrl=discoveryUrl)
+
+		spreadsheet_id = '1p1LNyQhNhDBNEOkYQPV9xcNRe60WDlmnuiPp78hxkIs'
+		value_input_option = 'RAW'
+		rangeName = 'Org Leadership Websites!F' + str(ContactSheetOutput.currentRow)
+		values = rows
+		body = {
+		      'values': values
+		}
+
+		try:
+			result = service.spreadsheets().values().update(spreadsheetId=spreadsheet_id, range=rangeName,
+		                                                valueInputOption=value_input_option, body=body).execute()
+		except:
+			print('Missed Row Output')
+		else:
+			ContactSheetOutput.currentRow += len(rows)
+
+		return result	
+
+	@staticmethod
+	def read_contact_output_records():
+	    """Google Sheets API Code.
+	    Pulls urls for all NFL Team RSS Feeds
+	    https://docs.google.com/spreadsheets/d/1p1LNyQhNhDBNEOkYQPV9xcNRe60WDlmnuiPp78hxkIs/
+	    """
+	    credentials = get_credentials()
+	    http = credentials.authorize(smgs.httplib2.Http())
+	    discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
+	                    'version=v4')
+	    service = smgs.discovery.build('sheets', 'v4', http=http,
+	                              discoveryServiceUrl=discoveryUrl)
+
+	    #specify sheetID and range
+	    spreadsheetId = '1p1LNyQhNhDBNEOkYQPV9xcNRe60WDlmnuiPp78hxkIs'
+	    rangeName = 'Samples!A' + str(ContactSheetOutput.initialRow) + ':N'
+	    result = service.spreadsheets().values().get(
+	        spreadsheetId=spreadsheetId, range=rangeName).execute()
+	    values = result.get('values', [])
+
+	    if not values:
+	        print('Record Output Ready: No Reocords')
+	    else:
+	        print('Record Output Ready')
+
+	    return values
+
+
+
