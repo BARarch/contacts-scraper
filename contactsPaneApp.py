02c3976f1fee4538768ae792a2bda1a81225fdf4
Anthony Quivers <anthonyquivers@gmail.com>
2018-03-19 09:49:36 -0400

    Start of Contacts Pane App

---
+++ contactsPaneApp.py
@@ -0,0 +1,16 @@
+import sys
+import time
+from tkinter import *
+import tkinter.ttk as ttk
+
+
+class Application(frame):
+    def __init__:
+        pass
+
+
+
+root = TK()
+app = Application(master=root)
+app.mainloop()
+root.destroy()
