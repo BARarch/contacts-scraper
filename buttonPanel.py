d8ce1391cb7e5a3247b1f00a6fa4981c0d3a81e9
Anthony Quivers <anthonyquivers@gmail.com>
2018-03-23 19:43:45 -0400

    Button Panel

--- buttonPanel.py
+++ buttonPanel.py
@@ -7,27 +7,51 @@ class ButtonPanel:
     def __init__(self, master=None, handler=None, buttonWidths=40, top=10, bottom=10):
         # Has a Frame
         self.frame = Frame(master)
-        self.frame.pack()
+        self.frame.pack(expand=True, fill=X, anchor=W)
         self.parent = master
         self.handler = handler
         
         self.dropDown = OptionMenu(self.frame, self.handler.scrapeSelection, 'All', 'Base', 'Today', 'Error')
         self.handler.scrapeSelection.set('Today') # set the default option
-        self.dropDown.pack(side=TOP)
+        self.dropDown.pack(side=TOP, anchor=W, expand=YES, fill=X)
         
         self.bottom = Frame(self.frame)
         self.bottom.pack(side=TOP)
-        self.QUIT = Button(self.bottom)
+        self.QUIT = Button(self.bottom, width=7)
         self.QUIT.configure(command=self.handler.handle_quit,
                                 text='QUIT',
                                 fg='red')
-        self.QUIT.pack(side=LEFT)
-        self.SCRAPE = Button(self.bottom)
+        self.QUIT.pack(side=LEFT, padx=2)
+        self.SCRAPE = Button(self.bottom, width=7)
         self.SCRAPE.configure(command=self.handler.handle_scrape,
                               text='Scrape',
                               fg='green',
                               state='disabled')
-        self.SCRAPE.pack(side=LEFT)
+        self.SCRAPE.pack(side=LEFT, padx=2)
+        
+    def enable_scrape(self):
+        self.SCRAPE.configure(state='active')
+        return self
+    
+    def disable_scrape(self):
+        self.SCRAPE.configure(state='disabled')
+        return self
+    
+    def enable_quit(self):
+        self.QUIT.configure(state='active')
+        return self
+    
+    def disable_quit(self):
+        self.QUIT.configure(state='disabled')
+        return self
+    
+    def enable_dropdown(self):
+        self.dropDown.configure(state='active')
+        return self
+    
+    def disable_dropdown(self):
+        self.dropDown.configure(state='disabled')
+        return self
         
     
     
@@ -48,10 +72,10 @@ if __name__ == '__main__':
             print(self.scrapeSelection.get())
             
         def handle_quit(self):
-            pass
+            self.buttons.enable_scrape().disable_quit().disable_dropdown()
         
         def handle_scrape(self):
-            pass
+            self.buttons.enable_quit().disable_scrape().enable_dropdown()
  
 
             
