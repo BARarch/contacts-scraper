97c64c8f419f53c73ed3ddff4e82c47714229076
Anthony Quivers <anthonyquivers@gmail.com>
2018-04-04 22:29:32 -0400

    Stubs for spreadsheed control object

--- ledRow.py
+++ ledRow.py
@@ -16,7 +16,8 @@ class LEDRow:
         
         self.indicator = LED(master=self.frame, appearance=FLAT, shape=ROUND, blink=blink, blinkrate=blinkrate, bd=0, outline='grey')
         self.indicator.frame.pack(side=LEFT, anchor=W, expand=YES, padx=5, pady=1)
-        self.name = Label(self.frame, text=name, anchor=W, width=LABELWIDTH).pack(side=LEFT,  fill=BOTH, pady=5)
+        self.name = Label(self.frame, text=name, anchor=W, width=LABELWIDTH)
+        self.name.pack(side=LEFT, expand=YES, anchor=W,  fill=BOTH, pady=5)
         self.status = Label(self.frame, relief=SUNKEN, bg=StatusColor, fg="red", text='', width=STATUSWIDTH)
         self.status.pack(expand=True, fill=Y, side=RIGHT,anchor=E)
         
@@ -66,8 +67,10 @@ class LEDRow:
 class LEDRowNoMsg(LEDRow):
     def __init__(self, master=None, name="NONE", blink=0, blinkrate=1):
         LEDRow.__init__(self, master=master, name=name, blink=blink, blinkrate=blinkrate)
-        self.status.configure(relief=FLAT, bg=master["bg"], text='',)
-        
+        self.frame.pack_forget()
+        self.status.pack_forget()
+        self.frame.pack(expand=True, side=TOP, anchor=W)
+              
     def waiting(self):
         self.indicator.blinkoff()
         self.indicator.alarm()
@@ -85,18 +88,75 @@ class LEDRowNoMsg(LEDRow):
         
     def message(self, msg):
         return self
+
+class LEDRowDiscription(LEDRow):
+    def __init__(self, master=None, name="NONE", blink=0, blinkrate=1):
+        LEDRow.__init__(self, master=master, name=name, blink=blink, blinkrate=blinkrate)
+        self.frame.pack_forget()
+        self.status.pack_forget()
+        self.frame.pack(expand=True, side=TOP, anchor=W)
+        self.bottomFrame = Frame(master)
+        self.bottomFrame.pack(expand=True, side=TOP, anchor=W)
+        self.discription = Label(self.bottomFrame, text='This is a lot of text I want it to wrap and wrap it will',
+                                                   wraplength=200,
+                                                   justify=LEFT,
+                                                   anchor=W)
+
+        self.discription.pack(anchor=W)
+
+    def message(self, msg):
+        self.name.configure(text=msg)
+        return self
+
+    def change_discription(self, msg):
+        self.discription.configure(text=msg)
+        return self
+
+    def waiting(self):
+        return self
+        
+    def ready(self):
+        return self
+
+    def active(self):
+        return self
+
+    def bad(self):
+        self.indicator.blinkoff()
+        self.indicator.alarm()
+        return self
+        
+    def good(self):
+        self.indicator.blinkoff()
+        self.indicator.turnon()
+        return self
+
+    def off(self):
+        self.indicator.blinkoff()
+        self.indicator.turnoff()
+        return self   
+
+
+
+        #wraplength=LABELWIDTH, justify=LEFT,
+
         
         
         
 if __name__ == '__main__':
+
+    paragraph = 'Welcome to the Gulp scraper.  I hope you are having a great day, and if not I hope I will make it better.'
     
     class TestLEDRow(Frame):
          def __init__(self, master=None):
             Frame.__init__(self)              # Do superclass init
             self.pack()
             LEDRow(self, name="The Test Row", blink=1).active().message('hellothere').stop_blink().off().ready().blink().waiting()
-            LEDRow(self, name="The Second Row", blink=1, blinkrate=2).waiting().stop_blink()
+            LEDRow(self, name="The Second Row", blink=1, blinkrate=2).waiting()
             LEDRow(self, name="The Third Row").ready().blink().stop_blink()
+            LEDRowNoMsg(self, name="No Msg")
+            LEDRowDiscription(self).bad().change_discription(paragraph).good().blink()
+            LEDRow(self, name="After Name Change").ready().blink().waiting()
             
     root = Tk()
     root.title('LED Row')
--- spreadSheetControl.py
+++ spreadSheetControl.py
@@ -6,31 +6,76 @@ import tkinter.ttk as ttk
 from ledRow import *
 
 class SpreadSheetControl:
-	def __init__(self, master=None, handler=None):
-		self.frame = Frame(master)
-		self.frame = master
-		self.buttonHandler = handler
-		self.frame.pack(side=TOP)
+
+    RestoreText = 'This restores all original contacts from the Backup Sheet.  If there are new contacts from the scraper they will be lost'
+    Paragraph = 'Welcome to the Gulp scraper.  I hope you are having a great day, and if not I hope I will make it better.'
+    
+    def __init__(self, master=None, handler=None):
+        self.frame = ttk.Notebook(master)
+        self.parent = master
+        self.handler = handler
+        self.frame.pack(side=TOP, expand=True, fill=X)
+
+        self.transferTab = Frame(self.frame)
+        self.restoreTab = Frame(self.frame)
+        
+        self.TRANSFER = Button(self.transferTab, width=12)
+        self.TRANSFER.configure(command=self.handler.handle_transfer,
+                               text='Transfer Scraped Contacts',
+                               wraplength=50,
+                               state='disabled')
+        self.TRANSFER.pack(side=LEFT, anchor=W, expand=True, fill=Y)
+        self.transferStatusFrame = ttk.Labelframe(self.transferTab, text='Transfer Status')
+        self.transferStatusFrame.pack(side=RIGHT, expand=True, fill=Y)
+        self.transferStatus = LEDRowDiscription(self.transferStatusFrame).bad().change_discription(SpreadSheetControl.Paragraph).good().blink()
+
+        Label(self.restoreTab, text=SpreadSheetControl.RestoreText, wraplength=200, justify=LEFT, anchor=W).pack(side=LEFT, anchor=W)
+        self.RESTORE = Button(self.restoreTab, width=12)
+        self.RESTORE.configure(command=self.handler.handle_restore,
+                               text='RESTORE CONTACTS',
+                               wraplength=70,
+                               state='disabled')
+        self.RESTORE.pack(side=RIGHT, anchor=E, expand=True, fill=Y)
+
+        self.frame.add(self.transferTab, text='Transfer', compound=TOP)
+        self.frame.add(self.restoreTab, text='Restore Backup')
+
+    def enable_transfer(self):
+        pass
+
+    def disable_transfer(self):
+        pass
+
+    def enable_restore(self):
+        pass
+
+    def disable_restore(self):
+        pass
+
+    def update_transfer_status(self, rowCounts):
+        pass
+
+
 
 
 
 
 if __name__ == '__main__':
 
-	class TestSpreadSheetControl(Frame):
-		def __init__(self, master=None):
-			Frame.__init__(self)
-			self.pack()
+    class TestSpreadSheetControl(Frame):
+        def __init__(self, master=None):
+            Frame.__init__(self)
+            self.pack()
 
-			self.manager = SpreadSheetControl(self, self)
+            self.manager = SpreadSheetControl(self, self)
 
-		def handle_transfer(self):
-			print('TRANSFER got pressed')
+        def handle_transfer(self):
+            print('TRANSFER got pressed')
 
-		def handle_restore(self):
-			pring('RESTORE got pressed')
+        def handle_restore(self):
+            pring('RESTORE got pressed')
 
-	root = Tk()
-	app = TestSpreadSheetControl(master=root)
-	app.mainloop()
-	root.destroy()
+    root = Tk()
+    app = TestSpreadSheetControl(master=root)
+    app.mainloop()
+    root.destroy()
