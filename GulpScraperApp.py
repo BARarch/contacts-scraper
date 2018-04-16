4481e0ee0b1513706ef655ff265876962c3f1e79
Anthony Quivers <anthonyquivers@gmail.com>
2018-04-15 21:24:18 -0400

    Icon File Set

--- GulpScraperApp.py
+++ GulpScraperApp.py
@@ -10,6 +10,10 @@ from mainAppControl import *
 if __name__ == '__main__':
           
     root = Tk()
+    root.title("Gulp Scraper")
+    img = PhotoImage(file='iconGulpPng.png')
+    root.tk.call('wm', 'iconphoto', root._w, img)
+    
     app = MainApplicationGD(master=root)
     app.after(500, app.startup)
     app.mainloop()
